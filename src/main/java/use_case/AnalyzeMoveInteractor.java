540fdef0a1f1e0abc6cabe1c2bc06a46c4c9a2bf
Sean Dursi <seanrobertdursi@gmail.com>
2026-07-15 10:57:39 -0400

    Implemented blackEval, whiteEval and boardEval to orchastrate them both with convertToFen

--- src/main/java/use_case/AnalyzeMoveInteractor.java
+++ src/main/java/use_case/AnalyzeMoveInteractor.java
@@ -1,19 +1,62 @@
 package use_case;
 
+import com.google.gson.JsonObject;
+import entity.Board;
+
 public class AnalyzeMoveInteractor{
-    private final EngineGateway engine;
+    private final ChessApiInterface ApiInterface;
 
-    public AnalyzeMoveInteractor(EngineGateway engine) {
-        this.engine = engine;
+    public AnalyzeMoveInteractor(ChessApiInterface ApiInterface) {
+        this.ApiInterface = ApiInterface;
     }
 
-    void analyze(String fen) throws Exception {
-        double eval = this.engine.evaluate(fen);
-        String bestMoveMessage = this.engine.bestMoveMessage(fen);
-
-        String bestMove = this.engine.bestMove(fen);
-        String from = bestMove.substring(0, 2);
-        String to = bestMove.substring(2, 4);
-        // Send this information to the presenter somehow or make into a move
+    /**
+     * Suggest the next best Move
+     * @param board, a Board object of the current board state
+     * @return a Move object of what should be done next
+     */
+    public String bestMove(Board board) throws Exception {
+        String fen = convertToFen(board);
+        JsonObject response = this.ApiInterface.request(fen);
+        return response.get("from").getAsString() + " -> " + response.get("to").getAsString();
     }
+
+    /**
+     * Get the current Board evaluation for whoever's turn it is
+     * @param board, a Board; snapshot of the current game
+     * @return a string displaying the appropriate player's Eval & WinChance
+     * @throws Exception maybe whe requesting api data
+     */
+    public String BoardEval(Board board) throws Exception {
+        String fen = convertToFen(board);
+        JsonObject response = this.ApiInterface.request(fen);
+        if (response.get("turn").getAsString().equals("w")){
+            return this.whiteEval(board);
+        } else { return this.blackEval(board); }
+
+    }
+
+    private String convertToFen(Board board) {
+        return "";
+    }
+
+    private String whiteEval(Board board) throws Exception{
+        String fen = convertToFen(board);
+        JsonObject response = this.ApiInterface.request(fen);
+        String result = "";
+        result += "White WinChance: " + response.get("winChance") + "\n";
+        result += "White Eval: " + response.get("eval");
+        return result;
+    }
+
+    private String blackEval(Board board) throws Exception{
+        String fen = convertToFen(board);
+        JsonObject response = this.ApiInterface.request(fen);
+        String result = "";
+        result += "Black WinChance: " + (-1) * (1 - response.get("winChance").getAsDouble()) + "\n";
+        result += "Black Eval: " + (-1) * response.get("eval").getAsDouble();
+        return result;
+    }
+
+
 }
