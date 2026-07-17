f8cc973274fa419b98f2a613678f757553f73c95
Sean Dursi <seanrobertdursi@gmail.com>
2026-07-17 11:14:52 -0400

    Seperate test file just for AnalyzeMoveInteractor

---
+++ src/main/test/java/interface_adapter/AnalyzeMoveInteractorTest.java
@@ -0,0 +1,28 @@
+package interface_adapter;
+
+import com.google.gson.JsonObject;
+import entity.Board;
+import org.junit.jupiter.api.Test;
+import use_case.AnalyzeMoveInteractor;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AnalyzeMoveInteractorTest {
+
+    private static final String START =
+            "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+    private static final Board BOARD = new Board(); // Update with data later
+
+
+    @Test
+    void nonEmptyNewTurnAnalysis() throws Exception {
+        ChessApiAdapter adapter = new ChessApiAdapter();
+        AnalyzeMoveInteractor interactor = new AnalyzeMoveInteractor(adapter);
+
+        String analysis = interactor.newTurnAnalysis(BOARD);
+
+        System.out.println(analysis);
+        assertNotNull(analysis);
+        assertFalse(analysis.isBlank());
+    }
+}
