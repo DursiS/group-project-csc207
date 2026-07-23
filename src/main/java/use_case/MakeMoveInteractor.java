be4e7f20f7cff7f662b1e32b207bda4b45d353b4
roan <roan.reynolds@mail.utoronto.ca>
2026-07-23 19:00:55 -0400

    added MakeMoveInteractor

---
+++ src/main/java/use_case/MakeMoveInteractor.java
@@ -0,0 +1,11 @@
+//this class is responsible for receiving the user's button press through some interface,
+// checking whether it is a valid move, and if so,
+//GameState owns the board
+
+package use_case;
+
+import entity.MoveValidator;
+
+public class MakeMoveInteractor {
+    private MoveValidator validator;
+}
