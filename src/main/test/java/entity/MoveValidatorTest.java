6f2ccb77f00152f0c0f379fce17bd199e540469d
roan <roan.reynolds@mail.utoronto.ca>
2026-07-23 17:10:48 -0400

    added ally obstruction MoveValidator test

--- src/main/test/java/entity/MoveValidatorTest.java
+++ src/main/test/java/entity/MoveValidatorTest.java
@@ -36,6 +36,23 @@ public class MoveValidatorTest {
         assertEquals(moves.size(), 10);
     }
 
+    @Test
+    void allyObstructionMoveValidatorTest(){
+        int[][] squares = new int[8][8];
+        squares[0][4] = 4;
+        squares[0][3] = 4;
+        Board b = new Board(squares, 0);//default board;
+        MoveValidator v = new MoveValidator(new BoardTopology(0,0));
+
+        ArrayList<Move> moves = v.getAllMoves(b);
+        System.out.println(moves.size());
+        for (int i = 0; i < moves.size(); i++) {
+            System.out.println( vectorToString(moves.get(i).getOrigin()) + " -> " + vectorToString(moves.get(i).getDestination()));
+        }
+        assertEquals(moves.size(), 20);
+    }
+
+
     static String vectorToString(int[] vector){
         return  "("+ Integer.toString(vector[0] ) + "," + Integer.toString(vector[1]) + ")";
     }
