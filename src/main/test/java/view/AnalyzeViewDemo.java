4fb5900f5f1f741ade428a62bbf7570002a2952f
Sean Dursi <seanrobertdursi@gmail.com>
2026-07-24 13:45:54 -0400

    Successful initial view demo, had to modify controller to be public.

--- src/main/test/java/view/AnalyzeViewDemo.java
+++ src/main/test/java/view/AnalyzeViewDemo.java
@@ -1,4 +1,30 @@
 package view;
 
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+import interface_adapter.AnalyzeController;
+import interface_adapter.AnalyzeViewModel;
+
 public class AnalyzeViewDemo {
+
+    /**
+     * Launches a small demo window for the analysis view.
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args) {
+        final AnalyzeViewModel viewModel = new AnalyzeViewModel();
+
+        final AnalyzeController controller = new AnalyzeController(() -> {});
+
+        final AnalyzeView view = new AnalyzeView(viewModel, controller);
+
+        final JFrame mainFrame = new JFrame("Analyze View");
+        mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        mainFrame.add(view);
+        mainFrame.pack();
+        mainFrame.setVisible(true);
+
+        viewModel.addMessage("Hello World");
+    }
 }
