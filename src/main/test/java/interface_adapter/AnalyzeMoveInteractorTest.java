81747caa64605c696176653e05a413cb05069208
Sean Dursi <seanrobertdursi@gmail.com>
2026-07-25 14:42:43 -0400

    Added a mock test for getting a turn analysis back

--- src/main/test/java/interface_adapter/AnalyzeMoveInteractorTest.java
+++ src/main/test/java/interface_adapter/AnalyzeMoveInteractorTest.java
@@ -1,9 +1,12 @@
 package interface_adapter;
 
+import com.google.gson.JsonObject;
 import entity.Board;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import use_case.AnalyzeMoveInteractor;
+import use_case.ChessApiInterface;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,14 +18,11 @@ public class AnalyzeMoveInteractorTest {
     private static Board OTHER_BOARD;
     private static AnalyzeMoveInteractor interactor;
 
-    @BeforeEach
-    void setUp() {
+    @BeforeAll
+    static void setUp() {
         ChessApiAdapter adapter = new ChessApiAdapter();
         AnalyzePresenter presenter = new AnalyzePresenter();
-        interactor = new AnalyzeMoveInteractor(
-                adapter,
-                presenter
-        );
+        interactor = new AnalyzeMoveInteractor(adapter, presenter);
 
         START_FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
         OTHER_FEN = "rnbqkbnr/ppp1pppp/8/8/3pP3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1";
@@ -42,9 +42,7 @@ public class AnalyzeMoveInteractorTest {
 
     @Test
     void nonEmptyNewTurnAnalysis() throws Exception {
-
-
-        String analysis = interactor.getFinalMessage(START_FEN);
+        String analysis = interactor.getAnalysisMessage(START_FEN);
 
         System.out.println(analysis);
         assertNotNull(analysis);
@@ -59,5 +57,36 @@ public class AnalyzeMoveInteractorTest {
         String convertedRandomFen = interactor
                 .convertToFen(OTHER_BOARD);
         assertEquals(OTHER_FEN, convertedRandomFen);
+    }
+
+    private static class MockChessApiInterface implements ChessApiInterface {
+
+        @Override
+        public JsonObject request(String fen){
+            JsonObject response = new JsonObject();
+
+            // random (but valid) info
+            response.addProperty("eval", 0.42);
+            response.addProperty("winChance", 45.14);
+            response.addProperty("from", "g8");
+            response.addProperty("to", "f6");
+            response.addProperty("move", "g8f6");
+            response.addProperty("text", "Move: Ng8 -> f6");
+            return response;
+        }
+    }
+
+    @Test
+    void mockApiInterfaceNonEmptyAnalysis() throws Exception{
+        MockChessApiInterface adapter = new MockChessApiInterface(); // Mock
+        AnalyzePresenter presenter = new AnalyzePresenter();
+        AnalyzeMoveInteractor mockInteractor = new AnalyzeMoveInteractor(adapter
+                , presenter);
+
+        String analysis = mockInteractor.getAnalysisMessage(START_FEN);
+
+        System.out.println(analysis);
+        assertNotNull(analysis);
+        assertFalse(analysis.isBlank());
     }
 }
