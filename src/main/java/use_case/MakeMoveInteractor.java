3295da24430faddf633c8a03f04f0a6483eb3b70
roan <roan.reynolds@mail.utoronto.ca>
2026-07-30 13:38:47 -0400

    adding clean architecture components...

--- src/main/java/use_case/MakeMoveInteractor.java
+++ src/main/java/use_case/MakeMoveInteractor.java
@@ -2,6 +2,19 @@
 // checking whether it is a valid move, and if so,
 //GameState owns the board
 
+
+//clean architecture components required for move:
+//view,
+// view model, controller, presenter
+// input boundary, output boundary (both interfaces) + input data, output data(both data objects)
+
+//no data access interface required. maybe pass something to gamestate for it to do that purpose...
+//
+
+
+
+
+
 package use_case;
 
 import entity.MoveValidator;
---
+++ src/main/java/view/MoveView.java
@@ -0,0 +1,13 @@
+package view;
+
+import interface_adapter.MoveViewModel;
+
+
+import javax.swing.*;
+import java.awt.*;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class MoveView extends JPanel implements PropertyChangeListener  {
+
+}
