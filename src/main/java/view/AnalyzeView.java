c349834804494213c83755a82b3c800d4d53daac
Sean Dursi <seanrobertdursi@gmail.com>
2026-07-31 12:35:48 -0400

    added formatted buttons no actions yet

--- src/main/java/view/AnalyzeView.java
+++ src/main/java/view/AnalyzeView.java
@@ -5,39 +5,85 @@ import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.io.IOException;
 
-import javax.swing.BoxLayout;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-import javax.swing.SwingUtilities;
+import javax.swing.*;
 
 import interface_adapter.AnalyzeController;
 import interface_adapter.AnalyzeViewModel;
 
 public class AnalyzeView extends JPanel implements PropertyChangeListener {
-    private static final int WIDTH = 200;
-    private static final int HEIGHT = 400;
-    private static final int TEXT_SIZE = 48;
+    private static final int WIDTH = 300;
+    private static final int HEIGHT = 600;
+    private static final int TITLE_SIZE = 24;
+    private static final int TEXT_SIZE = 16;
+    private static final int BUTTON_HEIGHT = 40;
+    private static final String FONT = "Arial";
 
-    private final JTextArea textArea = new JTextArea();
+    private JTextArea textArea;
     private final AnalyzeController controller;
 
     public AnalyzeView(AnalyzeViewModel viewModel, AnalyzeController controller) {
+        // Setup
         this.controller = controller;
-        // Subscribing
+        setupTextArea();
         viewModel.addPropertyChangeListener(this);
 
+        // Panel Config
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         setPreferredSize(new Dimension(WIDTH, HEIGHT));
         addDisplayLabel();
         setBackground(Color.PINK);
         add(new JScrollPane(this.textArea));
+        add(getButtonRow());
+    }
+
+    private JPanel getButtonRow() {
+        final JPanel buttonRow = new JPanel(new GridLayout(1, 2));
+        buttonRow.setBackground(Color.PINK);
+        buttonRow.add(getHistoryButton());
+        buttonRow.add(getReturnButton());
+        buttonRow.setMaximumSize(new Dimension(Integer.MAX_VALUE, BUTTON_HEIGHT));
+        return buttonRow;
+    }
+
+    private void setupTextArea() {
+        this.textArea = new JTextArea();
+        this.textArea.setEditable(false);
+        this.textArea.setLineWrap(true);
+        this.textArea.setWrapStyleWord(true);
+        this.textArea.setFont(new Font(FONT, Font.PLAIN, TEXT_SIZE));
+        this.textArea.setBackground(Color.PINK);
+    }
+
+    private JButton getHistoryButton() {
+        final JButton historyButton = new JButton("Message History");
+        historyButton.addActionListener(click -> {
+            // switch to one long history message
+        });
+        // button aesthetics
+        historyButton.setBackground(Color.PINK);
+        historyButton.setFont(new Font(FONT, Font.PLAIN, TEXT_SIZE));
+        historyButton.setForeground(Color.WHITE);
+        historyButton.setBorder(BorderFactory.createLineBorder(Color.GRAY));
+        historyButton.setVisible(true);
+        return historyButton;
+    }
+
+    private JButton getReturnButton() {
+        final JButton returnButton = new JButton("Return");
+        returnButton.addActionListener(click -> {
+            // switch to the most recent message of the list
+        });
+        returnButton.setBackground(Color.PINK);
+        returnButton.setFont(new Font(FONT, Font.PLAIN, TEXT_SIZE));
+        returnButton.setForeground(Color.WHITE);
+        returnButton.setBorder(BorderFactory.createLineBorder(Color.GRAY));
+        returnButton.setVisible(true);
+        return returnButton;
     }
 
     private void addDisplayLabel() {
         final JLabel displayLabel = new JLabel("Analysis Metrics");
-        displayLabel.setFont(new Font("Arial", Font.PLAIN, TEXT_SIZE));
+        displayLabel.setFont(new Font(FONT, Font.PLAIN, TITLE_SIZE));
         add(displayLabel);
     }
 
