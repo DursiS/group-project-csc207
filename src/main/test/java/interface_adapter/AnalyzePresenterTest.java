66c7306a231d6955470a3b63f1af2e4ce359bd8e
Sean Dursi <seanrobertdursi@gmail.com>
2026-08-05 11:09:36 -0400

    Presenter and ViewModel Tests, they pass.

---
+++ src/main/test/java/interface_adapter/AnalyzePresenterTest.java
@@ -0,0 +1,35 @@
+package interface_adapter;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import use_case.AnalyzeOutputData;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AnalyzePresenterTest {
+    static AnalyzeOutputData outputData;
+    static AnalyzeViewModel viewModel;
+    static AnalyzePresenter presenter;
+    static String formatted_message;
+
+    @BeforeAll
+    static void setUp() {
+        outputData = new AnalyzeOutputData(39.0,
+                0.49,
+                "e4",
+                "e5",
+                true
+        );
+        viewModel = new AnalyzeViewModel();
+        presenter = new AnalyzePresenter(viewModel);
+        formatted_message = "== WHITE'S TURN == \n\nWhite's Metrics: \n"
+                + "White WinChance: 39.0% \nWhite Eval: 0.49\n"
+                + "Best Move: e4 -> e5\n\n";
+    }
+
+    @Test
+    void testAnalyzePresenterMakeMessage() {
+        String message = presenter.makeMessage(outputData);
+        assertEquals(formatted_message, message);
+    }
+}
